ba42905b1599ae13a1337e7973d29c7d8ca532df
nickpark <nick.park1860@gmail.com>
2017-02-13 00:56:20 +0900

    CH3. 첫 단위 테스트와 url 맵핑 그리고 임시 뷰

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -3,8 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'lists.views.home_page', name='home'),
 )
