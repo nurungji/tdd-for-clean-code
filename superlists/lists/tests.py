d7a681c3b340633d67fbfc1e36c62828eba08ffb
nickpark <nick.park1860@gmail.com>
2017-02-19 21:28:52 +0900

    기능 테스트가 작업 아이템을 추가할 수 있는지 확인
    - ~p44

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,11 @@
-from django.core.urlresolvers import resolve
 from django.test import TestCase
+#
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1+1, 3)
+
+
+from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 
 from lists.views import home_page
