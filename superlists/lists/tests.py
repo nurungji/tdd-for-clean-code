280da38bb2fb3bca827f60ac472910a9602a5e28
nickpark <nick.park1860@gmail.com>
2017-02-19 22:01:22 +0900

    템플릿을 이용하도록 홈페이지 뷰를 리팩터링
    - ~p51

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,18 +7,26 @@ from django.test import TestCase
 
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-class HomePageTest(TestCase):
 
+class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest() #1
-        response = home_page(request) #2
-        self.assertTrue(response.content.startswith(b'<html>')) #3
-        self.assertIn(b'<title>To-Do lists</title>', response.content) #4
-        self.assertTrue(response.content.endswith(b'</html>')) #5
+        request = HttpRequest()  # 1
+        response = home_page(request)  # 2
+        print(repr(response.content))
+        self.assertTrue(response.content.startswith(b'<html>'))  # 3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  # 4
+        self.assertTrue(response.content.endswith(b'</html>'))  # 5
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
